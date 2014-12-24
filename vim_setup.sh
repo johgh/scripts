2260e18542aa771c049536d7b0e06eeba1c0c8a6
Jorge Hervás Vigueras <jorge@jhv-ele.(none)>
2014-12-24 17:04:01 +0100

    Added getgithub command and tested *setup.sh shells

--- vim_setup.sh
+++ vim_setup.sh
@@ -4,29 +4,25 @@ then
     # purge current vim version (if any)
     sudo apt-get purge vim-*
 
-    echo "deb http://ppa.launchpad.net/pkg-vim/vim-daily/ubuntu $1 main" | sudo tee -a /etc/apt/sources.list
-    # echo "deb http://ppa.launchpad.net/pi-rho/dev/ubuntu $1 main" | sudo tee -a  /etc/apt/sources.list
+    # echo "deb http://ppa.launchpad.net/pkg-vim/vim-daily/ubuntu $1 main" | sudo tee -a /etc/apt/sources.list
+    echo "deb http://ppa.launchpad.net/pi-rho/dev/ubuntu $1 main" | sudo tee -a  /etc/apt/sources.list
 
     # update and install gvim
     sudo apt-get update && sudo apt-get install vim vim-gnome
 fi
 
-cd ~
-
 # clean vim files
+cd ~
 rm -Rf .vim .vimrc .viminfo .vimbackup .vimswap .vimundo .vimviews
 
 # get .vim dir and submodules
 git clone --recursive https://github.com/johgh/vim .vim
+$HOME/.vim/bundle/YouCompleteMe/install.sh
+
+if [ ! -d $HOME/fonts ]
+    git clone https://github.com/powerline/fonts
+    $HOME/fonts/install
+fi
 
 # symlink .vimrc to $HOME
-ln -s $HOME/.vim/.vimrc .
-
-git clone https://github.com/powerline/fonts
-cd ~/fonts
-./install
-
-cd ~/.vim/bundle/YouCompleteMe/
-./install
-cd -
-
+ln -fs $HOME/.vim/.vimrc .
