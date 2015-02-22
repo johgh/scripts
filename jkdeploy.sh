91a51bd0a286d3dff57d31546da0eeb311eb0011
joh <jordihv@gmail.com>
2015-02-22 20:27:31 +0100

    exit jkdeploy if no message is provided

---
+++ jkdeploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+if [ -z $1 ]
+then
+    echo 'Provide message'
+    exit 1
+fi
+message=$1
+cd $HOME/johgh.io-source
+jekyll build
+cd source
+git add --all ./
+git commit -m "$message (deploy)"
+git push
+cd ..
+git add --all ./
+git commit -m "$message"
+git push
