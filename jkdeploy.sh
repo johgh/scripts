5b99dd63a769d67203420b3b68e483187b03cf1c
jorge <jhervas@direla.com>
2015-08-27 23:59:22 +0200

    push and jkdeploy modified

--- jkdeploy.sh
+++ jkdeploy.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 set -e
+
+# save current directory
+dir=`pwd`
+
 if [[ -z $1 ]]
 then
     echo 'Provide message'
     exit 1
 fi
+
 message=$1
+
 cd $HOME/johgh.io-source
 jekyll build
+
 cd source
 git add --all ./
 git commit -m "$message (deploy)"
-git push
+# executes push and sends pass automatically
+$dir/push
+
 cd ..
 git add --all ./
 git commit -m "$message"
