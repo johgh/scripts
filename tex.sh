0d1076fde99a74adf59cc8457ff8c43a3aa7d05d
jorge <jhervas@direla.com>
2016-08-19 18:16:24 +0200

    -

--- tex.sh
+++ tex.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 filename=`basename -s .md $1`
 kramdown -o latex --template document $filename.md > $filename.tex
-sed -i 's/scrartcl/scrbook/g' $filename.tex
+# especificamos formato Koma-Script Book y formato de página A5
+sed -i 's/{scrartcl}/[a5paper]{scrbook}/g' $filename.tex
+# Convertimos secciones en capítulos para que funcione la importación desde markdown
 sed -i 's/section/chapter/g' $filename.tex
-
