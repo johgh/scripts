ea7e8a7edb7ad08e4c61d6c7b4efc29207340204
Jorge Hervás Vigueras <jordihv@gmail.com>
2016-09-12 12:32:16 +0200

    added tex templates explanation

--- createbook.sh
+++ createbook.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 usage() { echo "$(basename $0) [-f <a4|a5>] [-m <pagebreaks|cont>] [-j <book|dual>] [-i <header|footer|body>] <filename>.md
-opciones:
+- opciones:
     FORMATO
         -f a5 (modo por defecto): Formato de página A5 (el modo por defecto, -m, es 'pagebreak')
         -f a4: Formato de página A4 (el modo por defecto, -m, es 'cont')
@@ -11,7 +11,11 @@ opciones:
         -j dual: junta 2 páginas en cada A4 con ordenación estándar
         -j book: pone 2 páginas en cada A4 en formato folleto
     INCLUIR
-        -i especifica que partes se incluyen en el documento (se pueden escoger varias especificando varias veces el parámetro)" 1>&2; exit 1; }
+        -i especifica que partes se incluyen en el documento (se pueden escoger varias especificando varias veces el parámetro)
+
+- plantillas .tex (a incluir en el mismo directorio que el fichero .md):
+        - header.tex: páginas iniciales del documento en formato latex
+        - footer.tex: páginas finales del documento en formato latex" 1>&2; exit 1; }
 
 dependencies() {
     if ! $(which pdflatex &>/dev/null); then sudo apt-get install texlive texlive-latex-recommended texlive-latex-extra psutils; fi
@@ -21,7 +25,8 @@ dependencies() {
     if ! $(which evince &>/dev/null); then sudo apt-get install evince; fi
 }
 
-# parámetros por defecto
+##### PARSEO DE PARÁMETROS RECIBIDOS #####
+# definición de parámetros por defecto
 f=a5
 j=default
 m=default
@@ -31,6 +36,7 @@ included+=('header')
 included+=('footer')
 included+=('body')
 
+# parámetros recibidos
 while getopts ":f:i:j:m:" o; do
 case "${o}" in
     f)
@@ -56,7 +62,7 @@ case "${o}" in
 esac
 done
 
-# si se han especificado por parametro partes a incluir sobreescrimos las de por defecto
+# si se han especificado por parametro partes a incluir sobreescrimos las que vienen por defecto
 if [ ! -z $included_args ]; then
     included=(${included_args[*]})
 fi
@@ -67,16 +73,17 @@ if [[ ! -f $@ || -z $@ || ! $(echo $@ | grep '.md') ]]; then
     usage
 fi
 
+
+##### PREPARACIÓN ENTORNO #####
 # comprobación de dependencias
 dependencies
-
 filename=`basename -s .md $@`
 dirname=`dirname $@`
-
-# echo $filename
-# exit
-
+# cambio al directorio de trabajo
 cd "$dirname"
+# limpieza de ficheros generados en ejecuciones anteriores
+rm "$filename".pdf "$filename".ps 2> /dev/null
+
 
 ##### CONVERSIÓN DE KRAMDOWN A LATEX #####
 kramdown -o latex --template document "$filename".md > "$filename".tex
@@ -92,14 +99,32 @@ sed -i 's/-/---/g' "$filename".tex
 # espacio vertical
 sed -i 's/> > >/\\bigskip/g' "$filename".tex
 
+
 ##### INCLUIMOS PARTES SELECCIONADAS SEGÚN PARÁMETROS #####
 sed -n '/\\begin{document}/,/\\end{document}/P' "$filename".tex | head -n-1 | tail -n+2 > body.tex
 
-cat header.tex > "$filename".tex
-if [[ " ${included[@]} " =~ " header " ]]; then cat intro.tex >> "$filename".tex; fi
-if [[ " ${included[@]} " =~ " body " ]]; then cat body.tex >> "$filename".tex; fi
-if [[ " ${included[@]} " =~ " footer " ]]; then cat footer.tex >> "$filename".tex; fi
+# añadimos cabecera (latex)
+cat <<EOF > "$filename".tex
+\documentclass
+\usepackage[utf8x]{inputenc}
+\usepackage[T1]{fontenc}
+\usepackage{listings}
+\setcounter{footnote}{0}
+\makeatletter
+\@ifundefined{date}{}{\date{}}
+\makeatother
+\renewcommand{\contentsname}{{ÍNDICE}}
+\renewcommand{\partname}{Parte}
+\begin{document}
+EOF
+
+if [[ " ${included[@]} " =~ " header " ]]; then cat header.tex >> "$filename".tex 2> /dev/null; fi
+if [[ " ${included[@]} " =~ " body " ]]; then cat body.tex >> "$filename".tex 2> /dev/null; fi
+if [[ " ${included[@]} " =~ " footer " ]]; then cat footer.tex >> "$filename".tex 2> /dev/null; fi
+
+# añadimos marca de fin de documento (latex)
 echo '\end{document}' >> "$filename".tex
+
 
 ##### FORMATO A4/A5 SEGÚN PARÁMETROS #####
 bookstructure() {
@@ -131,6 +156,7 @@ if [[ "$m" == 'default' ]]; then
     fi
 fi
 
+
 ##### FORMATO ARTÍCULO/LIBRO SEGÚN PARÁMETROS #####
 if [[ "$m" == 'cont' ]]; then
     sed -i 's/\\documentclass.*$/\\documentclass['"$f"'paper]{scrartcl}/g' "$filename".tex
@@ -142,26 +168,36 @@ if [[ "$m" == 'pagebreaks' ]]; then
     bookstructure
 fi
 
+
 ##### CONVERSIÓN A FICHERO FINAL #####
+notvalidparts() {
+    echo -e "Included parts not found or invalid syntax: $included\nPlease include your header.tex and/or footer.tex file in the same directory as your .md file"
+    exit 1
+}
+
 if [[ "$j" == 'default' ]]; then
     # se compila 2 veces para obtener TOC actualizada
-    pdflatex -draftmode "$filename".tex
-    pdflatex "$filename".tex
-    evince "$filename".pdf
+    pdflatex -draftmode "$filename".tex >/dev/null 2>&1
+    pdflatex "$filename".tex >/dev/null 2>&1
+    if [[ ! -f "$filename".pdf ]]; then notvalidparts; fi
+    nohup evince "$filename".pdf >/dev/null 2>&1 &
 else
     # se compila 2 veces para obtener TOC actualizada
-    pdflatex -draftmode -output-format dvi "$filename".tex
-    pdflatex -draftmode -output-format dvi "$filename".tex
-    dvips "$filename".dvi
+    pdflatex -draftmode -output-format dvi "$filename".tex >/dev/null 2>&1
+    pdflatex -draftmode -output-format dvi "$filename".tex >/dev/null 2>&1
+    if [[ ! -f "$filename".dvi ]]; then notvalidparts; fi
+    dvips -q "$filename".dvi
     mv "$filename".ps "$filename"_temp.ps
-    if [[ "$f" == 'a5' ]]; then resize=' -p a4 -s1 '; else resize=' -p a4 '; fi
+    if [[ "$f" == 'a5' ]]; then paramspsn=' -q -p a4 -s1 '; else paramspsn=' -q -p a4 '; fi
     if [[ "$j" == 'dual' ]]; then
-        cat "$filename"_temp.ps | psnup $resize -2 > "$filename".ps
+        cat "$filename"_temp.ps | psnup $paramspsn -2 > "$filename".ps
     else
-        psbook -s4 "$filename"_temp.ps | psnup $resize -2 > "$filename".ps
+        psbook -q -s4 "$filename"_temp.ps | psnup $paramspsn -2 > "$filename".ps
     fi
-    okular "$filename".ps
+    nohup okular "$filename".ps >/dev/null 2>&1 &
 fi
-# limpieza
+
+
+##### LIMPIEZA DE FICHEROS TEMPORALES #####
 rm *.toc *.dvi *.aux *.out *.log "$filename"_temp.ps "$filename".tex body.tex 2> /dev/null
 
