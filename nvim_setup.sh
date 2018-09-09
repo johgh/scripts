67b905733ea95c59cfea412242b41f9f4e1b08ed
Jorge Hervás Vigueras <jordihv@gmail.com>
2018-09-09 10:17:50 +0200

    added script automove + nvim_setup cross-platform

--- nvim_setup.sh
+++ nvim_setup.sh
@@ -1,49 +1,16 @@
 #!/bin/bash
-dependencies="exuberant-ctags wmctrl build-essential cmake python-dev php-codesniffer silversearcher-ag xsel"
-dists="trusty utopic vivid wily xenial yakkety"
+# NOTES:
+# some distros require xclip/xsel package for clipboard support
+# exuberant-ctags is ctags in some distros
 
-CHECK_DEPS=$HOME/bin/check_deps
-
-$CHECK_DEPS "$dependencies"
+# this script is now cross-platform (Debian/Arch), so we don't check packages
+# dependencies="exuberant-ctags wmctrl build-essential cmake python-dev php-codesniffer silversearcher-ag xsel"
+# CHECK_DEPS=$HOME/bin/check_deps
+# $CHECK_DEPS "$dependencies"
 
 # install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-
-if [[ -f ~/.vimrc || -d ~/.vim ]]
-then
-    read -n 1 -s -p $'Previous installation detected. Do you want to continue overwriting previous installation? [y/N]\n' option
-    if [ $option != 'y' ]
-    then
-        echo 'Installation canceled by user'
-        exit 1
-    fi
-fi
-
-i=0
-array[0]="Don't install Vim (install only .vim directory)"
-echo "[$i] $array"
-for distro in $dists
-do
-    (( i ++ ))
-    echo "[$i] $distro"
-    array[$i]="$distro"
-done
-
-read -s -n 1 -p $'Select your distro for installing Vim:\n' option
-
-if [[ $option != 0  && $option != '' ]]
-then
-    # purge current Vim version (if any)
-    sudo apt purge neovim
-
-    echo "deb http://ppa.launchpad.net/neovim-ppa/unstable/ubuntu ${array[${option}]} main" | sudo tee -a  /etc/apt/sources.list
-
-    # update and install neovim
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8231B6DD
-    sudo apt update
-    sudo apt install neovim
-fi
 
 # clean Vim files
 cd ~
@@ -54,17 +21,6 @@ git clone --recursive https://github.com/johgh/vim .vim
 ln -s ~/.vim ~/.config/nvim
 nvim +PlugClean! +qall
 nvim +PlugInstall +qall
-
-# if [ -f $HOME/.vim/bundle/YouCompleteMe/install.sh ]
-# then
-#     $HOME/.vim/bundle/YouCompleteMe/install.sh
-# fi
-
-# if [ ! -d $HOME/fonts ]
-# then
-#     git clone https://github.com/powerline/fonts
-#     $HOME/fonts/install
-# fi
 
 cp -R $HOME/.vim/fonts/* $HOME/.local/share/fonts
 fc-cache -f $HOME/.local/share/fonts
